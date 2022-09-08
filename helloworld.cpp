faffaf5f5484789548614205ab3cd78b06365a22
Ritesh Kumar <stackritesh@gmail.com>
2022-09-08 14:48:12 +0530

    new change in dev

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
     int main(){
 
-        cout<<"Hello World";
+        cout<<"Hello World\n";
 
-        cout<<"in dev branch";
+        cout<<"in dev branch\n";
+
+        cout<<"new change in dev branch\n";
 
         return 0;
     }
